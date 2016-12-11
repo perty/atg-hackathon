c5fc56da08806ec6c3db3000fa73849f6f9dafa4
Per Lundholm <per@artcomputer.se>
2016-12-11 10:11:51 +0100

    Created a canary test.

---
+++ fitnesse/fixtures/src/main/java/se/atg/hackathon/fixtures/ShouldIBuyMilk.java
@@ -0,0 +1,26 @@
+package se.atg.hackathon.fixtures;
+
+@SuppressWarnings("unused")
+public class ShouldIBuyMilk {
+    private int dollars;
+    private int pints;
+    private boolean creditCard;
+
+    public void setCashInWallet(int dollars) {
+        this.dollars = dollars;
+    }
+
+    public void setPintsOfMilkRemaining(int pints) {
+        this.pints = pints;
+    }
+
+    public void setCreditCard(String valid) {
+        creditCard = "yes".equals(valid);
+    }
+
+    public String GoToStore() {
+        return (pints == 0 && (dollars > 2 || creditCard)) ? "yes" : "no";
+    }
+
+
+} 
